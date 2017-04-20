46e37492afd3f997a794415a7be72cce9cab4c9f
willyaugusto <willyaugusto@gmail.com>
2017-04-19 22:25:32 -0300

    Aula 07 - Ok

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,13 @@
+angular.module('alurapic').controller('FotosController', function($scope, $http) {
+
+    $scope.fotos = [];
+    $scope.filtro = '';
+
+    $http.get('v1/fotos')
+    .success(function(fotos) {
+        $scope.fotos = fotos;
+    })
+    .error(function(erro) {
+        Console.log(erro);
+    });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,19 @@
-angular.module('alurapic', []);
+angular.module('alurapic', ['minhasDiretivas', 'ngAnimate', 'ngRoute'])
+
+    .config(function($routeProvider, $locationProvider) {
+
+        $locationProvider.html5Mode(true);
+
+        $routeProvider.when('/fotos', {
+            templateUrl: 'partials/principal.html',
+            controller: 'FotosController'
+        });
+
+        $routeProvider.when('/fotos/new', {
+            templateUrl: 'partials/foto.html',
+            controller: 'FotoController'
+        });
+
+        $routeProvider.otherwise({redirectTo: '/fotos'});
+
+    });
